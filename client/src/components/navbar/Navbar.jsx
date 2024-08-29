91f87a0d1a7c511eb28fff02fc43a79412977df7
prateeksha sharma <prateekshasharma128@gmail.com>
2024-08-29 21:38:38 +0530

    designed home page and sidebar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,23 +1,43 @@
-import "./Navbar.scss"
+import { useState } from "react";
+import "./Navbar.scss";
 function Navbar() {
-    return (
-      <nav>
-        <div className='left'>
-          <a href='/' className="logo">
-            <img src='/logo.png' alt='logo' />
-            <span>HomeHunt</span>
-          </a>
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <nav>
+      <div className='left'>
+        <a href='/' className='logo'>
+          <img src='/logo.png' alt='logo' />
+          <span>HomeHunt</span>
+        </a>
+        <a href='/'>Home</a>
+        <a href='/'>About</a>
+        <a href='/'>Contact</a>
+        <a href='/'>Agents</a>
+      </div>
+      <div className='right'>
+        <a href='/'>Login</a>
+        <a href='/' className='signup'>
+          SignUp
+        </a>
+        <div className='menuIcon'>
+          <img
+            src='./menu.png'
+            alt='sidebar-icon'
+            onClick={() => setIsOpen((isOpen) => !isOpen)}
+          />
+        </div>
+
+        <div className={isOpen ? "menu active" : "menu"}>
           <a href='/'>Home</a>
           <a href='/'>About</a>
           <a href='/'>Contact</a>
           <a href='/'>Agents</a>
-        </div>
-        <div className='right'>
           <a href='/'>Login</a>
-          <a href='/' className="signup">SignUp</a>
+          <a href='/'>SignUp</a>
         </div>
-      </nav>
-    );
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
