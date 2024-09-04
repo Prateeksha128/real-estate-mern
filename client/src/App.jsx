aa2dbb4814f1a2d40a721476b5b7eb1903f448cf
prateeksha sharma <prateekshasharma128@gmail.com>
2024-09-04 15:39:20 +0530

    added chat, profile,login ,register, updateProfile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,9 @@ import Homepage from "./routes/homepage/Homepage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./routes/layout/Layout";
 import SinglePage from "./routes/singlepage/SinglePage";
+import ProfilePage from "./routes/profilePage/ProfilePage";
+import Login from "./routes/login/Login";
+import Register from "./routes/register/Register";
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
           <Route path='/' element={<Homepage />} />
           <Route path='/list' element={<ListPage />} />
           <Route path='/:id' element={<SinglePage />} />
+          <Route path='/profile' element={<ProfilePage />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
         </Route>
       </Routes>
     </BrowserRouter>
